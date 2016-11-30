b333e1ffbfed10e57003059b7decd2e4b3ed9088
Raul Palacios <palacios.raul02@gmail.com>
2016-11-30 17:56:17 -0600

    Estructura lista n.n

---
+++ 2. Variables/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+ // var x string = "Mamá!! ya puedo utilizar variables en go" 
+
+ x := "Mamá!! ya puedo utilizar variables en go" 
+
+
+ fmt.Println(x)
+}
